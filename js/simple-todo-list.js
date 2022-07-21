59d15a1f5873c1d385f041d713255d95944602fc
MopsterV <zdanya0@gmail.com>
2022-07-21 13:43:06 +0300

    123

--- js/simple-todo-list.js
+++ js/simple-todo-list.js
@@ -1,51 +1,65 @@
-const todo = {
-  action(e) {
-    const target = e.target;
-    if (target.classList.contains('todo__action')) {
-      const action = target.dataset.todoAction;
-      const elemItem = target.closest('.todo__item');
-      if (action === 'deleted' && elemItem.dataset.todoState === 'deleted') {
-        elemItem.remove();
-      } else {
-        elemItem.dataset.todoState = action;
-      }
-      this.save();
-    } else if (target.classList.contains('todo__add')) {
-      this.add();
-      this.save();
-    }
-  },
-  add() {
-    const elemText = document.querySelector('.todo__text');
-    if (elemText.disabled || !elemText.value.length) {
-      return;
-    }
-    document.querySelector('.todo__items').insertAdjacentHTML('beforeend', this.create(elemText.value));
-    elemText.value = '';
-  },
-  create(text) {
-    return `<li class="todo__item" data-todo-state="active">
-      <span class="todo__task">${text}</span>
-      <span class="todo__action todo__action_restore" data-todo-action="active"></span>
-      <span class="todo__action todo__action_complete" data-todo-action="completed"></span>
-      <span class="todo__action todo__action_delete" data-todo-action="deleted"></span></li>`;
-  },
-  init() {
-    const fromStorage = localStorage.getItem('todo');
-    if (fromStorage) {
-      document.querySelector('.todo__items').innerHTML = fromStorage;
-    }
-    document.querySelector('.todo__options').addEventListener('change', this.update);
-    document.addEventListener('click', this.action.bind(this));
-  },
-  update() {
-    const option = document.querySelector('.todo__options').value;
-    document.querySelector('.todo__items').dataset.todoOption = option;
-    document.querySelector('.todo__text').disabled = option !== 'active';
-  },
-  save() {
-    localStorage.setItem('todo', document.querySelector('.todo__items').innerHTML);
-  }
+const addTaskBtn = document.getElementById('add-tsk-btn');
+const deskTaskInput = document.getElementById('description-task');
+const todosWrapper = document.querySelector('.todo__items');
+
+let tasks;
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+
+let todoItemElems = []
+
+function Task(description) {
+    this.description = description;
+    this.completed = false;
 }
 
-todo.init();
+createTemplate = (task, index) => {
+    return `
+    <div class="todo__li ${task.completed ? 'checked' : ''}" >
+        <input id="${index}"  onclick="completeTask(${index})" class="btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}>
+        <div class="description"><label for="${index}">${task.description}</label></div>
+        <button onclick="deleteTask(${index})" class="btn-delete"></button>
+    </div>
+    `
+}
+
+
+const fillHtmlList = () => {
+    todosWrapper.innerHTML = "";
+    if(tasks.length > 0) {
+        tasks.forEach((item, index) => {
+            todosWrapper.innerHTML += createTemplate(item, index);
+        });
+        todoItemElems = document.querySelectorAll('.todo__text');
+    }
+};
+
+fillHtmlList();
+
+const updateLocal = () => {
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+};
+
+const completeTask = index => {
+    console.log(index);
+    tasks[index].completed = !tasks[index].completed;
+    if(tasks[index].completed) {
+        todoItemElems[index].classList.add("checked");  
+    } else {
+        todoItemElems[index].classList.remove("checked");
+    }
+    updateLocal();
+    fillHtmlList();
+};
+
+addTaskBtn.addEventListener('click', () => {
+    tasks.push(new Task(deskTaskInput.value));
+    updateLocal();
+    fillHtmlList();
+    deskTaskInput.value = '';
+});
+
+const deleteTask = index => {
+        tasks.splice(index, 1);
+        updateLocal();
+        fillHtmlList();
+};
